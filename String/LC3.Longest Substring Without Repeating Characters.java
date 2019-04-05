c5edc58e7f54857b2f9a3df829011ce9c78e7c54
cathy651 <cathyweiyiliu@gmail.com>
2019-04-04 22:39:12 -0700

    Update LC3.Longest Substring Without Repeating Characters.java

--- String/LC3.Longest Substring Without Repeating Characters.java
+++ String/LC3.Longest Substring Without Repeating Characters.java
@@ -1,1 +1,27 @@
-code start here
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // initial
+        int i = 0, j = 0, maxLength = 0;
+        Set<Character> set = new HashSet<>();
+        // ij 两个指针，j 开始扫描，
+        while(i < s.length() && j < s.length()){
+            if(!set.contains(s.charAt(j))){
+                // 如果set 里面没有j 就把j 放入set,然后继续往后扫描j, 同时更新目标长度
+                set.add(s.charAt(j));
+                j++;
+                maxLength = Math.max(maxLength, j - i);
+            }else{
+                //遇到重复，先删除当前的i 所在元素（因为你要找的是连续不重复，所以重复元素之前的都必须删除）
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return maxLength;
+    }
+}
+
+// 这道题目如果要输出longest substring， 
+//for (String str : set) {  
+//       System.out.println(str);  
+// }  
