b72ad4d4cce4428f9f39f3133576704710326f35
cathy651 <cathyweiyiliu@gmail.com>
2019-04-04 22:39:56 -0700

    Update LC3.Longest Substring Without Repeating Characters.java

--- String/LC3.Longest Substring Without Repeating Characters.java
+++ String/LC3.Longest Substring Without Repeating Characters.java
@@ -1,3 +1,5 @@
+3. Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
